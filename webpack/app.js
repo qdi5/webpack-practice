6c4eb6c70a3db74f9116aa48e99242b147569004
qdi5 <969296206@qq.com>
2020-04-05 19:42:28 +0800

    css，字体，自动注入html文件

--- webpack/app.js
+++ webpack/app.js
@@ -1,6 +1,11 @@
 // 使用垫片脚本实现es6的内置功能（如Promise,）
 // import "@babel/polyfill" (When setting `useBuiltIns: 'usage'`, polyfills are automatically imported when needed.
 // Please remove the `import '@babel/polyfill'` call or use `useBuiltIns: 'entry'` instead.)
+import './main.css'
+import CSS from  './app.scss'
+import './fonts/iconfont.css'
+
+console.log('CSS', CSS)
 const a = 1
 let b =2
 var c = () => 2
@@ -24,6 +29,7 @@ data()
 import oneImage from './one.jpg'
 var img = new Image()
 img.src = oneImage;
-var dom = document.getElementById("images");
+var dom = document.getElementById("app");
+dom.classList.add(CSS.avatar);
 dom.append(img)
 console.log(oneImage);
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,13 @@
-const path = require('path')
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 function resolve(dir) {
   return path.resolve(__dirname, dir)
 }
 module.exports = {
   mode: "development",
   entry: {
-    "app": "./app.js"
+    app: "./app.js",
   },
   output: {
     // 修改打包后输出的目录
@@ -18,25 +20,63 @@ module.exports = {
         // 配置babel转义es6+为es5代码
         test: /\.js$/,
         loader: "babel-loader",
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.tsx?$/,
         loader: "ts-loader",
-        exclude: "/node_modules/"
+        exclude: "/node_modules/",
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: {
-          loader: 'url-loader',
+          loader: "url-loader",
           options: {
             name: "[name]-[hash:6].[ext]",
             outputPath: "images/",
-            limit: 1024 * 7
-          }
-        }
-      }
+            limit: 1024 * 7,
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              // 使用模块化的方式使用css，避免当前组件的样式影响其他组件的样式
+              modules: {
+                localIdentName: "[path][name]__[local]--[hash:base64:5]",
+              },
+            },
+          },
+          "sass-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(woff|woff2|svg|eot|ttf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "fonts/",
+              name: "[name]-[hash:6].[ext]",
+            },
+          },
+        ],
+      },
     ],
-
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
 };
